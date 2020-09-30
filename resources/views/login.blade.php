b575767ac77d0321d3632df3afb305994f94f9a8
Đỗ Hồng Sơn <57494582+sondhph9397@users.noreply.github.com>
2020-09-30 19:31:56 +0700

    sondh

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <header>
-        <h1 class="header">
-            header login
-        </h1>
-    </header>
-        
+@extends('student-layout.login-layout')
+@section('title', 'Đăng nhập')
+@section('header','Mời Đăng Nhập')
+@section('content')
     <form action="{{route('post-login')}}" method="POST">
         @csrf
         <input type="text" name="username" placeholder=username><br>
@@ -21,7 +9,5 @@
         <button type="submit">login</button>
     </form>
 
-    <footer>footer login</footer>
-</body>
-
-</html>
+@endsection
+@section('footer','Footer extends login')
