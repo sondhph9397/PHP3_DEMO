067639cc5872721401fc919b53ed316b04db27dd
Đỗ Hồng Sơn <57494582+sondhph9397@users.noreply.github.com>
2020-10-04 18:59:59 +0700

    ok

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Student::all(); //lấy ra hết hoặc $students = Student::orderBy('id','desc')->get();
+        return view('test-admin', ['list' => $students]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        dd('Student Controller create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        // nếu chỉ truyền vào $student -> chỉ nhận đc id của student
+        $studentObjEloquentModel = Student::find($student);
+        $studentObjQuery = DB::table('students')->find($student);
+        dd($studentObjEloquentModel, $studentObjQuery);
+        // nếu truyền Student $student -> thực hiện truy vấn tìm student có id
+        $studentObj = $student;
+        return view('students.show',['students'=>$studentObj]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Student $student)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\StudentController;
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use PhpParser\Node\Expr\FuncCall;
 // dùng request để nhận dữ liêu gửi lên
 use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,60 +21,65 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::view('/admin','test-admin');
 
+//tạo route resouce cho student controller
+Route::resource('students', StudentController::class);
+
+//tạo roure cho student không dùng resouce
+// Route::get('students',[StudentController::class, 'index'])->name('students.index')
 
 
 
+// STUDENT
 
-Route::get('/students', function () {
-    //sử dụng query builer 
-    // return DB::table('students')->where('id','=',26)->get();
-    //lấy ra mảng students
-    // $students = DB::table('students')->get();
-    //lấy ra riêng 1 students
-    $students = DB::table('students')->find(26);
+// Route::get('/students', function () {
+//     //sử dụng query builer 
+//     // return DB::table('students')->where('id','=',26)->get();
+//     //lấy ra mảng students
+//     // $students = DB::table('students')->get();
+//     //lấy ra riêng 1 students
+//     $students = DB::table('students')->find(26);
 
-    //truyền vào mảng [tên biến view nhận đc => giá trị $students]
-    return view('students.detail', ['studentsValue' => $students]);
-});
+//     //truyền vào mảng [tên biến view nhận đc => giá trị $students]
+//     return view('students.detail', ['studentsValue' => $students]);
+// });
 
 
-//giá trị truyền vài tương ứng với tham số function
-Route::get('/students/{id}/{age}', function ($id, $number) {
-    dd('gia tri truyen vao url la: ' . $id . ' va ' . $number);
-});
+// //giá trị truyền vài tương ứng với tham số function
+// Route::get('/students/{id}/{age}', function ($id, $number) {
+//     dd('gia tri truyen vao url la: ' . $id . ' va ' . $number);
+// });
 
-Route::get('/students/detail', function () {
-    return view('students.detail');
-});
+// Route::get('/students/detail', function () {
+//     return view('students.detail');
+// });
 
-//cách số 2:
-Route::view('/students/detail-2', 'students.detail');
+// //cách số 2:
+// Route::view('/students/detail-2', 'students.detail');
 
-//tạo đường dẫn và truy vấn
-Route::get('/students-list', function () {
-    //truy vấn
-    $students = DB::table('students')->orderBy('id', 'desc')->get();
+// //tạo đường dẫn và truy vấn
+// Route::get('/students-list', function () {
+//     //truy vấn
+//     $students = DB::table('students')->orderBy('id', 'desc')->get();
 
-    return view('students.list', ['list' => $students]);
-})->name('students-list');
+//     return view('students.list', ['list' => $students]);
+// })->name('students-list');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('get-login');
+// Route::get('/login', function () {
+//     return view('login');
+// })->name('get-login');
 
-Route::post('/post-login', function (Request $request) {
-    // dd($request->all());
-    $username = $request->username;
-    //thực hiện truy vấn theo giá trị vừa gửi lên
-    $students = DB::table('students')->where('name', 'like', "%$username%")->first();
-    //nếu có student thì sẽ rediẻct sang student-list
-    if ($students) {
-        return redirect()->route('students-list');
-    }
-    // nếu không thì quay lại login
-    return redirect()->route('get-login');
+// Route::post('/post-login', function (Request $request) {
+//     // dd($request->all());
+//     $username = $request->username;
+//     //thực hiện truy vấn theo giá trị vừa gửi lên
+//     $students = DB::table('students')->where('name', 'like', "%$username%")->first();
+//     //nếu có student thì sẽ rediẻct sang student-list
+//     if ($students) {
+//         return redirect()->route('students-list');
+//     }
+//     // nếu không thì quay lại login
+//     return redirect()->route('get-login');
     
-})->name('post-login');
+// })->name('post-login');
